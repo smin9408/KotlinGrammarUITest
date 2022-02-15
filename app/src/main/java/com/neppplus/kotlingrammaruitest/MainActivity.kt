ded5d548ffd2159791453dd0b1f2d2ef24d22abc
smin9408 <stmdaks888@naver.com>
2022-02-15 15:55:32 +0900

    로그인 로직 완성

--- app/src/main/java/com/neppplus/kotlingrammaruitest/MainActivity.kt
+++ app/src/main/java/com/neppplus/kotlingrammaruitest/MainActivity.kt
@@ -2,10 +2,32 @@ package com.neppplus.kotlingrammaruitest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnLogin.setOnClickListener {
+
+            val id = "admin"
+            val password = "asdf"
+
+            val inputId = loginID.text.toString()
+            val inputPs = loginPS.text.toString()
+
+            if (id == inputId && password == inputPs) {
+                Toast.makeText(this, "관리자님 환영합니다.", Toast.LENGTH_SHORT).show()
+            } else if (id == inputId) {
+                Toast.makeText(this, "비밀번호가 틀렸습니다", Toast.LENGTH_SHORT).show()
+            } else if (password == inputPs) {
+                Toast.makeText(this, "아이디가 틀렸습니다", Toast.LENGTH_SHORT).show()
+            }
+
+        }
+
+
     }
 }
